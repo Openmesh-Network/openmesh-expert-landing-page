e18d966975a9832840dfcc3bd76efcafa83c5b7c
Bruno Laureano <brunolsantos152@gmail.com>
2023-09-15 10:04:24 -0300

    fix: fixing the about link

--- app/head.tsx
+++ app/head.tsx
@@ -1,10 +1,10 @@
 export default function Head() {
   return (
     <>
-      <title>OpenMesh Academy</title>
+      <title>Openmesh Academy</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="L3A Ecosystem" />
-      <link rel="icon" href="/images/l3a.ico" />
+      <meta name="description" content="Openmesh Academy" />
+      <link rel="icon" href="/openmesh-logo.png" />
     </>
   )
 }
